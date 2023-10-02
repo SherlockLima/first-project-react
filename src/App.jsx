bd5e9fb1a35f23ffdc2582274875b75587db5d1d
Jean gonçalves Lima <cbjeanlima@gmail.com>
2023-10-02 18:03:46 -0300

    :tada: feat: Aprendendo sobre components reutilizavéis

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-import Card from './components/Card'
+import Card from './components/Card/Card'
+import swPosterImg from "./assets/sw-poster.jpg"
+import esbPosterImg from "./assets/esb-poster.jpg"
+import rotjPosterImg from "./assets/rotj-poster.jpg"
 
 function App() {
 
   return (
-      <div >
-        <Card />
-      </div>
+    <>
+      <h1>Exercício 2</h1>
+      <Card title="Pôster: Star Wars (1977)" poster={swPosterImg} />
+      <Card title="Pôster: Empire Strikes Back (1980)" poster={esbPosterImg} />
+      <Card title="Pôster: Return of the Jedi (1983)" poster={rotjPosterImg} />
+    </>
   )
 }
 
